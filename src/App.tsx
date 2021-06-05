1fc5142ab11eba1d2626f854063b3553e81db253
DI CUI <orka.cuidi@gmail.com>
2021-06-05 10:29:57 +0800

    add query client

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,22 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
+import { QueryClientProvider } from 'react-query';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { queryClient } from './queryClient';
 
 export const App = () => {
   const { t } = useTranslation();
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          {t('title')}
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+          <Grid minH="100vh" p={3}>
+            <ColorModeSwitcher justifySelf="flex-end" />
+            {t('title')}
+          </Grid>
+        </Box>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 };

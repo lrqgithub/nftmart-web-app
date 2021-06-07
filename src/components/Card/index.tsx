ffdd54fc1201d89075fad10d350e0cf5055facc8
DI CUI <orka.cuidi@gmail.com>
2021-06-07 08:29:52 +0800

    renaming

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from 'react';
+import { Box, Heading, Divider } from '@chakra-ui/react';
+import { Colors } from '../../constants';
+
+export const CardHead: FC = ({ children }) => (
+  <Box paddingX={4} display="inline-block">
+    <Box paddingY={4}>
+      <Heading as="h4" size="md" color={Colors.TextBlack}>
+        {children}
+      </Heading>
+    </Box>
+  </Box>
+);
+
+type CardBodyProps = {
+  bodyPadding: boolean;
+}
+
+export const CardBody: FC<CardBodyProps> = ({ children, bodyPadding }) => (
+  <Box paddingX={bodyPadding ? 4 : 0}>
+    <Box paddingY={bodyPadding ? 4 : 0}>{children}</Box>
+  </Box>
+);
+
+export interface CardProps {
+  title?: React.ReactNode;
+  icon?: string;
+  body?: React.ReactNode;
+  bodyPadding?: boolean;
+  noHeadBorder?: boolean;
+}
+
+const Card: FC<CardProps> = ({
+  title,
+  icon,
+  noHeadBorder = false,
+  body,
+  bodyPadding = true,
+  children,
+  ...restProps
+}) => (
+  <Box borderRadius="3px" backgroundColor="white" padding={0} {...restProps}>
+    {icon && (
+    <Box as="img" src={icon} w="22px" h="22px" marginLeft="20px" display="inline-block" />
+    )}
+    {title && <CardHead>{title}</CardHead>}
+    {title && !noHeadBorder && <Divider borderColor={Colors.DividerDark} />}
+    {body || <CardBody bodyPadding={bodyPadding}>{children}</CardBody>}
+  </Box>
+);
+
+export default Card;
---
+++ src/components/Link/index.tsx
@@ -0,0 +1,59 @@
+// import { Link as RouterLink } from 'react-router-dom';
+import {
+  Text, Link, HTMLChakraProps, LinkProps,
+} from '@chakra-ui/react';
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { Colors } from '../../constants';
+
+export interface NLinkProps extends HTMLChakraProps<'p'> {
+  path: string;
+  title: string;
+  active?: boolean;
+  bordered?: boolean;
+  linkProps?: LinkProps;
+}
+
+const NLink: FC<NLinkProps> = (props) => {
+  const { t } = useTranslation();
+  const {
+    path, title, active = false, bordered = false, linkProps, ...restStyles
+  } = props;
+
+  const borderBottom = {
+    content: '" "',
+    height: 1,
+    width: '80%',
+    borderRadius: 3,
+    position: 'absolute',
+    backgroundColor: Colors.Primary,
+    left: '50%',
+    bottom: -2,
+    transform: 'translate(-50%, -50%)',
+  };
+
+  return (
+    <Text
+      fontSize={16}
+      _hover={{
+        color: Colors.Primary,
+      }}
+      position="relative"
+      {...restStyles}
+    >
+      <Link
+        // as={RouterLink}
+        key={title}
+        to={path}
+        color={active ? Colors.Primary : ''}
+        _after={active && bordered ? borderBottom : {}}
+        {...linkProps}
+      >
+        {t(title)}
+      </Link>
+    </Text>
+  );
+};
+
+export default NLink;

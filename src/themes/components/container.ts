348640162ca396f4e3652e48e1ef05e5907d39e0
lrqgithub <1819899940@qq.com>
2021-06-29 11:27:40 +0800

    chore: fix themes style

---
+++ src/themes/components/container.ts
@@ -0,0 +1,10 @@
+import { ComponentStyleConfig } from '@chakra-ui/react';
+
+const Container: ComponentStyleConfig = {
+  baseStyle: {
+    maxWidth: '1360px',
+    paddingX: '0',
+  },
+};
+
+export default Container;
